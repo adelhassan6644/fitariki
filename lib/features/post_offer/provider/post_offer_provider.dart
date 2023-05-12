30c8d03e4f304ef0f6c8cd11afe910f393da6302
adelhassan6644 <110145987+adelhassan6644@users.noreply.github.com>
2023-05-12 17:36:03 +0300

    update(post offer): -add reset function to post offer provider

--- lib/features/post_offer/provider/post_offer_provider.dart
+++ lib/features/post_offer/provider/post_offer_provider.dart
@@ -7,10 +7,12 @@ import '../../maps/models/address_model.dart';
 import '../repo/post_offer_repo.dart';
 
 class PostOfferProvider extends ChangeNotifier {
-
   PostOfferRepo postOfferRepo;
   final ScheduleProvider scheduleProvider;
-  PostOfferProvider({required this.postOfferRepo, required this.scheduleProvider,});
+  PostOfferProvider({
+    required this.postOfferRepo,
+    required this.scheduleProvider,
+  });
 
   AddressModel? startLocation;
   onSelectStartLocation(v) {
@@ -70,7 +72,7 @@ class PostOfferProvider extends ChangeNotifier {
   }
 
   List<String> followers = ["محمد احمد", "لؤي احمد", "محمد الفيصل"];
-  List<String> selectedFollowers = ["محمد احمد", "لؤي احمد", "محمد الفيصل"];
+  List<String> selectedFollowers = [];
   onSelectFollow(v, index) {
     if (v) {
       selectedFollowers.add(followers[index]);
@@ -80,89 +82,100 @@ class PostOfferProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  checkData() {
-      if (startLocation == null) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار الصورةالشخصية!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (scheduleProvider.selectedDays.isEmpty) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار الايام!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (startTime == null) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء ادخال الاسم الثلاثي!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (endTime != startTime) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار العمر!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (startDate != null) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار العمر!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (endDate != startDate) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار العمر!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (minPrice != null) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار العمر!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (maxPrice != null) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار العمر!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      if (endLocation == null) {
-        CustomSnackBar.showSnackBar(
-            notification: AppNotification(
-                message: "برجاء اختيار الحنسية!",
-                isFloating: true,
-                backgroundColor: ColorResources.IN_ACTIVE,
-                borderColor: Colors.transparent));
-        return;
-      }
-      return true;
+  reset() {
+    startLocation = null;
+    endLocation = null;
+    startTime = DateTime.now();
+    endTime = DateTime.now();
+    startDate = DateTime.now();
+    endDate = DateTime.now();
+    minPrice = null;
+    maxPrice = null;
+    selectedFollowers.clear();
   }
 
+  checkData() {
+    if (startLocation == null) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار الصورةالشخصية!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (scheduleProvider.selectedDays.isEmpty) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار الايام!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (startTime == null) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء ادخال الاسم الثلاثي!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (endTime != startTime) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار العمر!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (startDate != null) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار العمر!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (endDate != startDate) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار العمر!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (minPrice != null) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار العمر!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (maxPrice != null) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار العمر!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    if (endLocation == null) {
+      CustomSnackBar.showSnackBar(
+          notification: AppNotification(
+              message: "برجاء اختيار الحنسية!",
+              isFloating: true,
+              backgroundColor: ColorResources.IN_ACTIVE,
+              borderColor: Colors.transparent));
+      return;
+    }
+    return true;
+  }
 }
