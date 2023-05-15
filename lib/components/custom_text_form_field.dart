78da23a518f11b0cc2e4071fd72443b172e21e11
adelhassan6644 <110145987+adelhassan6644@users.noreply.github.com>
2023-05-15 18:20:56 +0300

    update text form field

--- lib/components/custom_text_form_field.dart
+++ lib/components/custom_text_form_field.dart
@@ -12,11 +12,8 @@ class CustomTextFormField extends StatelessWidget {
   final Widget? prefixWidget;
   final bool label;
   final TextInputType? inputType;
-  final Function(String?)? onSave;
   final String? Function(String?)? valid;
   final AutovalidateMode? validationMode;
-  final IconData? icon;
-  final IconData? eIcon;
   final TextEditingController? controller;
   final VoidCallback? onTap;
   final void Function(String)? onChanged;
@@ -26,31 +23,30 @@ class CustomTextFormField extends StatelessWidget {
   final Color? pIconColor;
   final FocusNode? focus;
   final bool? read;
-  final bool? flag;
   final VoidCallback? edit;
-  final bool? isEdit;
 
   final List<TextInputFormatter>? formatter;
-  final double? hor;
   final int? maxLength;
   final Color? fieldColor;
   final int? maxLine;
   final int? minLine;
-  final bool isValidat;
+  final TextInputAction keyboardAction;
+  final AutovalidateMode autoValidateMode;
+  final TextAlign? textAlign;
+  final void Function(String?)? onSaved;
 
   const CustomTextFormField({
     super.key,
+    this.keyboardAction = TextInputAction.next,
+    this.autoValidateMode = AutovalidateMode.onUserInteraction,
     this.prefixWidget,
     this.initialValue,
-    this.isValidat = true,
     this.maxLine = 1,
     this.minLine = 1,
     this.hint,
     this.sufWidget,
-    this.onSave,
     this.onTap,
     this.onChanged,
-    this.icon,
     this.inputType,
     this.valid,
     this.controller,
@@ -58,11 +54,7 @@ class CustomTextFormField extends StatelessWidget {
     this.sIcon,
     this.label = false,
     this.read,
-    this.eIcon,
     this.edit,
-    this.isEdit,
-    this.flag,
-    this.hor,
     this.validationMode,
     this.formatter,
     this.maxLength,
@@ -70,11 +62,17 @@ class CustomTextFormField extends StatelessWidget {
     this.pSvgIcon,
     this.pIconColor,
     this.fieldColor,
+    this.textAlign,
+    this.onSaved,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onFieldSubmitted: onSaved,
+      textAlign: textAlign ?? TextAlign.start,
+      autovalidateMode: autoValidateMode,
+      textInputAction: keyboardAction,
       onTap: onTap,
       validator: valid,
       controller: controller,
@@ -83,123 +81,128 @@ class CustomTextFormField extends StatelessWidget {
       focusNode: focus,
       readOnly: read == true ? true : false,
       maxLines: maxLine,
-      minLines: minLine??1,
+      minLines: minLine ?? 1,
       keyboardType: inputType,
-      inputFormatters: inputType == TextInputType.phone ? [
-        FilteringTextInputFormatter.allow(RegExp('[0-9]'))
-      ] : formatter,
-      onSaved: (onSave),
-      style: !isValidat ? AppTextStyles.w400.copyWith(color: ColorResources.PRIMARY_COLOR,
-          fontSize: 14
-      ) : AppTextStyles.w500.copyWith(
-          color: ColorResources.SECOUND_PRIMARY_COLOR,
-          fontSize: 14
-      ),
+      inputFormatters: inputType == TextInputType.phone
+          ? [FilteringTextInputFormatter.allow(RegExp('[0-9]'))]
+          : formatter,
+      style: AppTextStyles.w500
+          .copyWith(color: ColorResources.SECOUND_PRIMARY_COLOR, fontSize: 14),
       cursorColor: ColorResources.SECOUND_PRIMARY_COLOR,
       onChanged: onChanged,
       decoration: InputDecoration(
         counterText: "",
-        prefixIcon:  Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.w,),
-          child: prefixWidget ?? (pAssetIcon != null ? Image.asset(
-            pAssetIcon!,
-            height: 22.h,
-            color: pIconColor??ColorResources.DISABLED,
-          ) :pSvgIcon != null?
-          customImageIconSVG(
-            imageName:pSvgIcon!,
-            color:pIconColor?? Colors.black,
-            height: 22.h,):null),
-
-
+        prefixIcon: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 8.w,
+          ),
+          child: prefixWidget ??
+              (pAssetIcon != null
+                  ? Image.asset(
+                      pAssetIcon!,
+                      height: 22.h,
+                      color: pIconColor ?? ColorResources.DISABLED,
+                    )
+                  : pSvgIcon != null
+                      ? customImageIconSVG(
+                          imageName: pSvgIcon!,
+                          color: pIconColor ?? Colors.black,
+                          height: 22.h,
+                        )
+                      : null),
         ),
-        focusedBorder: read == true ?  const OutlineInputBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(Dimensions.RADIUS_DEFAULT,),
-          ),
-          borderSide: BorderSide(
-              color: ColorResources.LIGHT_BORDER_COLOR,
-              width: 1 ,
-              style: BorderStyle.solid
-          ),): const OutlineInputBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(Dimensions.RADIUS_DEFAULT,),
-          ),
-          borderSide: BorderSide(
-              color: ColorResources.SECOUND_PRIMARY_COLOR,
-              width: 1 ,
-              style: BorderStyle.solid
-          ),),
+        focusedBorder: read == true
+            ? const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    Dimensions.RADIUS_DEFAULT,
+                  ),
+                ),
+                borderSide: BorderSide(
+                    color: ColorResources.LIGHT_BORDER_COLOR,
+                    width: 1,
+                    style: BorderStyle.solid),
+              )
+            : const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    Dimensions.RADIUS_DEFAULT,
+                  ),
+                ),
+                borderSide: BorderSide(
+                    color: ColorResources.SECOUND_PRIMARY_COLOR,
+                    width: 1,
+                    style: BorderStyle.solid),
+              ),
         border: const OutlineInputBorder(
           borderRadius: BorderRadius.all(
-            Radius.circular(Dimensions.RADIUS_DEFAULT,),
+            Radius.circular(
+              Dimensions.RADIUS_DEFAULT,
+            ),
           ),
           borderSide: BorderSide(
               color: ColorResources.LIGHT_BORDER_COLOR,
-              width: 1 ,
-              style: BorderStyle.solid
-          ),),
+              width: 1,
+              style: BorderStyle.solid),
+        ),
         disabledBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(
-            Radius.circular(Dimensions.RADIUS_DEFAULT,),
+            Radius.circular(
+              Dimensions.RADIUS_DEFAULT,
+            ),
           ),
           borderSide: BorderSide(
               color: ColorResources.LIGHT_BORDER_COLOR,
-              width: 1 ,
-              style: BorderStyle.solid
-          ),),
+              width: 1,
+              style: BorderStyle.solid),
+        ),
         enabledBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(
-            Radius.circular(Dimensions.RADIUS_DEFAULT,),
+            Radius.circular(
+              Dimensions.RADIUS_DEFAULT,
+            ),
           ),
           borderSide: BorderSide(
               color: ColorResources.LIGHT_BORDER_COLOR,
-              width: 1 ,
-              style: BorderStyle.solid
-          ),),
+              width: 1,
+              style: BorderStyle.solid),
+        ),
         errorBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(
-            Radius.circular(Dimensions.RADIUS_DEFAULT,),
+            Radius.circular(
+              Dimensions.RADIUS_DEFAULT,
+            ),
           ),
           borderSide: BorderSide(
               color: ColorResources.FAILED_COLOR,
-              width: 1 ,
-              style: BorderStyle.solid
-          ),),
+              width: 1,
+              style: BorderStyle.solid),
+        ),
         focusedErrorBorder: const OutlineInputBorder(
           borderRadius: BorderRadius.all(
-            Radius.circular(Dimensions.RADIUS_DEFAULT,),
+            Radius.circular(
+              Dimensions.RADIUS_DEFAULT,
+            ),
           ),
           borderSide: BorderSide(
               color: ColorResources.FAILED_COLOR,
-              width: 1 ,
-              style: BorderStyle.solid
-          ),),
-        contentPadding: EdgeInsets.symmetric(vertical: 10.h, horizontal: sufWidget != null ? 0 : 8.w),
+              width: 1,
+              style: BorderStyle.solid),
+        ),
+        contentPadding: EdgeInsets.symmetric(
+            vertical: 10.h, horizontal: sufWidget != null ? 0 : 8.w),
         isDense: true,
         alignLabelWithHint: true,
         hintText: hint,
-        hintStyle: isValidat ? AppTextStyles.w400.copyWith(
-            color: ColorResources.DISABLED,
-            fontSize: 14
-        ) : AppTextStyles.w400.copyWith(
-            color: ColorResources.FAILED_COLOR,
-            fontSize: 14
-        ),
+        hintStyle: AppTextStyles.w400
+            .copyWith(color: ColorResources.DISABLED, fontSize: 14),
         labelText: label ? hint : null,
         fillColor: ColorResources.FILL_COLOR,
-        floatingLabelStyle: isValidat ? AppTextStyles.w400.copyWith(
-            color: ColorResources.SECOUND_PRIMARY_COLOR,
-            fontSize: 11
-        ) : AppTextStyles.w400.copyWith(
-            color: ColorResources.FAILED_COLOR,
-            fontSize: 11
-        ),
+        floatingLabelStyle: AppTextStyles.w400.copyWith(
+            color: ColorResources.SECOUND_PRIMARY_COLOR, fontSize: 11),
         filled: true,
-        errorStyle: AppTextStyles.w400.copyWith(
-            color: ColorResources.FAILED_COLOR,
-            fontSize: 11
-        ),
+        errorStyle: AppTextStyles.w400
+            .copyWith(color: ColorResources.FAILED_COLOR, fontSize: 11),
         prefixIconConstraints: BoxConstraints(maxHeight: 25.h),
       ),
     );
