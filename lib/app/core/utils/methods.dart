a8575424209c18d355d058e0f415f2303ce2e0fe
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-06-17 03:06:00 +0300

    update days count

--- lib/app/core/utils/methods.dart
+++ lib/app/core/utils/methods.dart
@@ -81,31 +81,17 @@ abstract class Methods {
   static getDayCount({
     required DateTime date,
   }) {
-    int weeks = 0;
-    int days = 0;
-    int hour = 0;
-
-    if (DateTime.now().isAfter(date)) {
-      weeks = date.weekday - DateTime.now().weekday;
-      days = date.day - DateTime.now().day;
-      if (weeks >= 1) {
-        return "$weeks اسبوع";
-      } else if (days >= 1) {
-        return "$days يوم";
-      } else {
-        hour = date.hour - DateTime.now().hour;
-        if (hour <= 1) {
-          return "ساعة";
-        } else if (hour <= 2) {
-          return "ساعتين";
-        } else if (3 <= hour && hour <= 10) {
-          return "$hour ساعات";
-        } else if (hour > 10) {
-          return "$hour ساعة";
-        }
-      }
-    } else {
+    Duration difference = DateTime.now().difference(date);
+    if (difference.inSeconds < 5) {
       return "الآن";
+    } else if (difference.inMinutes < 1) {
+      return "${difference.inSeconds} ثانية";
+    } else if (difference.inHours < 1) {
+      return "${difference.inMinutes} دقيقة";
+    } else if (difference.inHours < 24) {
+      return "${difference.inHours} ساعة";
+    } else {
+      return "${difference.inDays} يوم";
     }
   }
 
