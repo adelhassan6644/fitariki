30211996d182b725cf2059340ff358611e3a5f0a
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-12-13 18:57:42 +0200

    update local notification

--- lib/app/notifications/notification_helper.dart
+++ lib/app/notifications/notification_helper.dart
@@ -13,7 +13,6 @@ import '../../data/config/di.dart';
 import '../../features/home/provider/home_provider.dart';
 import '../../features/my_rides/model/my_rides_model.dart';
 import '../../features/my_trips/provider/my_trips_provider.dart';
-import '../../features/notifications/model/notifications_model.dart';
 import '../../features/notifications/provider/notifications_provider.dart';
 import '../../features/profile/provider/profile_provider.dart';
 import '../../navigation/custom_navigation.dart';
